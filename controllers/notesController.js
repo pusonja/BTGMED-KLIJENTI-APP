d9e4bf39afcb049702d6597e695712430c475ba9
Darko <pusonja@gmail.com>
2023-10-31 12:36:04 +0100

    utorak, greska sa Notes

---
+++ controllers/notesController.js
@@ -0,0 +1,123 @@
+const Note = require('../models/Note')
+const User = require('../models/User')
+const asyncHandler = require('express-async-handler')
+
+// @desc Get all notes 
+// @route GET /notes
+// @access Private
+const getAllNotes = asyncHandler(async (req, res) => {
+    // Get all notes from MongoDB
+    const notes = await Note.find().lean()
+
+    // If no notes 
+    if (!notes?.length) {
+        return res.status(400).json({ message: 'Nema unesenih podataka o klijentima' })
+    }
+
+    // Add username to each note before sending the response 
+    // See Promise.all with map() here: https://youtu.be/4lqJBBEpjRE 
+    // You could also do this with a for...of loop
+    const notesWithUser = await Promise.all(notes.map(async (note) => {
+        const user = await User.findById(note.user).lean().exec()
+        return { ...note, username: user.username }
+    }))
+
+    res.json(notesWithUser)
+})
+
+// @desc Create new note
+// @route POST /notes
+// @access Private
+const createNewNote = asyncHandler(async (req, res) => {
+    const { user, title, text } = req.body
+
+    // Confirm data
+    if (!user || !title || !text) {
+        return res.status(400).json({ message: 'Sva polja su obavezna!' })
+    }
+
+    // Check for duplicate title
+    const duplicate = await Note.findOne({ title }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Dupliciran naslov unosa!' })
+    }
+
+    // Create and store the new user 
+    const note = await Note.create({ user, title, text })
+
+    if (note) { // Created 
+        return res.status(201).json({ message: 'Novi zapis o klijentu kreiran!' })
+    } else {
+        return res.status(400).json({ message: 'Pogresni podaci prilikom unosa!' })
+    }
+
+})
+
+// @desc Update a note
+// @route PATCH /notes
+// @access Private
+const updateNote = asyncHandler(async (req, res) => {
+    const { id, user, title, text, completed } = req.body
+
+    // Confirm data
+    if (!id || !user || !title || !text || typeof completed !== 'boolean') {
+        return res.status(400).json({ message: 'Sva polja su obavezna' })
+    }
+
+    // Confirm note exists to update
+    const note = await Note.findById(id).exec()
+
+    if (!note) {
+        return res.status(400).json({ message: 'Zapis o klijentu nije pronadjen' })
+    }
+
+    // Check for duplicate title
+    const duplicate = await Note.findOne({ title }).lean().exec()
+
+    // Allow renaming of the original note 
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: 'Dupliciran naziv unos' })
+    }
+
+    note.user = user
+    note.title = title
+    note.text = text
+    note.completed = completed
+
+    const updatedNote = await note.save()
+
+    res.json(`'${updatedNote.title}' updated`)
+})
+
+// @desc Delete a note
+// @route DELETE /notes
+// @access Private
+const deleteNote = asyncHandler(async (req, res) => {
+    const { id } = req.body
+
+    // Confirm data
+    if (!id) {
+        return res.status(400).json({ message: 'Potreban ID zapisa o klijentu' })
+    }
+
+    // Confirm note exists to delete 
+    const note = await Note.findById(id).exec()
+
+    if (!note) {
+        return res.status(400).json({ message: 'Zapis o klijentu nije pronadjen' })
+    }
+
+    const result = await note.deleteOne()
+
+    const reply = `Unos '${result.title}' sa ID ${result._id} je obrisan`
+
+    res.json(reply)
+})
+
+module.exports = {
+    getAllNotes,
+    createNewNote,
+    updateNote,
+    deleteNote
+}
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,74 +1,80 @@
 const User = require('../models/User')
 const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
-const bcrypt = require ('bcrypt')
+const bcrypt = require('bcrypt')
 
 // @desc Get all users
 // @route GET /users
 // @access Private
-const getAllUsers = asyncHandler(async (req,res) => {
+const getAllUsers = asyncHandler(async (req, res) => {
+    // Get all users from MongoDB
     const users = await User.find().select('-password').lean()
-    if(!users) {
-        return res.status(400).json({ message: 'Ne postoji korisnik!'})
+
+    // If no users 
+    if (!users?.length) {
+        return res.status(400).json({ message: 'No users found' })
     }
+
     res.json(users)
 })
 
 // @desc Create new user
 // @route POST /users
 // @access Private
-const createNewUser = asyncHandler(async (req,res) => {
-    const { username, password, roles} = req.body
+const createNewUser = asyncHandler(async (req, res) => {
+    const { username, password, roles } = req.body
 
-    //Confirm data
-    if(!username || !password || !Array.isArray(roles) || !roles.length) {
-        return res.status(400).json({ message: 'Sva polja obavezna!'})
+    // Confirm data
+    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({ message: 'All fields are required' })
     }
 
-    //Check for duplicates
+    // Check for duplicate username
     const duplicate = await User.findOne({ username }).lean().exec()
 
     if (duplicate) {
-        return res.status(409).json({ message: 'Korisnicko ime vec postoji!'})
+        return res.status(409).json({ message: 'Duplicate username' })
     }
 
-    //Hash password
+    // Hash password 
     const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
 
-    const userObject = { username, "password": hashedPwd, roles}
+    const userObject = { username, "password": hashedPwd, roles }
 
-    //Create and store new user
+    // Create and store new user 
     const user = await User.create(userObject)
 
-    if (user) { //created
-        res.status(201).json({ message: 'Novi korisnik ${username} kreiran'}) 
+    if (user) { //created 
+        res.status(201).json({ message: `New user ${username} created` })
     } else {
-        res.status(400).json({ message:'Nepravilni korisnicki podaci'})
+        res.status(400).json({ message: 'Invalid user data received' })
     }
 })
 
 // @desc Update a user
 // @route PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req,res) => {
-    const { id, username, roles, active, status, password} = req.body
+const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, roles, active, password } = req.body
 
-    // Confirm data
-    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active !=='boolean') {
-        return res.status(400).json({ message:'Sva polja su obavezna!'})
+    // Confirm data 
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({ message: 'All fields except password are required' })
     }
 
+    // Does the user exist to update?
     const user = await User.findById(id).exec()
 
     if (!user) {
-        return res.status(400).json({ message:'Korisnik nije pronadjen!'})
+        return res.status(400).json({ message: 'User not found' })
     }
 
-    // Check for duplicate
+    // Check for duplicate 
     const duplicate = await User.findOne({ username }).lean().exec()
-    // Allow updates for the original user
+
+    // Allow updates to the original user 
     if (duplicate && duplicate?._id.toString() !== id) {
-        return res.status(409).json({ message:'Duplo korisnicko ime'})
+        return res.status(409).json({ message: 'Duplicate username' })
     }
 
     user.username = username
@@ -76,39 +82,42 @@ const updateUser = asyncHandler(async (req,res) => {
     user.active = active
 
     if (password) {
-        //Hash password
-        user.password = await bcrypt.hash(password, 10) //salt rounds
+        // Hash password 
+        user.password = await bcrypt.hash(password, 10) // salt rounds 
     }
 
-    const updateUser =  await user.save()
+    const updatedUser = await user.save()
 
-    res.json({ message:'${updateUser.username} updated'})
+    res.json({ message: `${updatedUser.username} updated` })
 })
 
 // @desc Delete a user
 // @route DELETE /users
 // @access Private
-const deleteUser = asyncHandler(async (req,res) => {
+const deleteUser = asyncHandler(async (req, res) => {
     const { id } = req.body
 
+    // Confirm data
     if (!id) {
-        return res.status(400).json({ message:'User ID potreban!'})
+        return res.status(400).json({ message: 'User ID Required' })
     }
 
-    const notes = await Note.findOne({ user: id }).lean().exec()
-    if (notes?.lenght) {
-        return res.status(400).json({ message:'Korisnik je unosio podatke!'})
+    // Does the user still have assigned notes?
+    const note = await Note.findOne({ user: id }).lean().exec()
+    if (note) {
+        return res.status(400).json({ message: 'User has assigned notes' })
     }
 
+    // Does the user exist to delete?
     const user = await User.findById(id).exec()
 
     if (!user) {
-        return res.status.json({ message:'Korisnik nije pronadjen!'})
+        return res.status(400).json({ message: 'User not found' })
     }
-    
+
     const result = await user.deleteOne()
 
-    const reply = 'Korisnik ${result.username} sa ID ${result._id} je obrisan!'
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
 
     res.json(reply)
 })
